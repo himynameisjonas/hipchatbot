0335002b1f3d6da46136257ef2e72514c2aed927
Jonas Forsberg <himynameisjonas@gmail.com>
2011-09-08 16:09:08 +0200

    That's what she said!

---
+++ lib/commands/twss.rb
@@ -0,0 +1,13 @@
+class Twss < Bot::Command
+  monitor_all
+  
+  require 'twss'
+  TWSS.threshold = 1.0
+  def self.respond(message)
+    message.send("That's what she said!") if TWSS(message.message)
+  end
+
+  def self.description
+    "That's what she said!"
+  end
+end
