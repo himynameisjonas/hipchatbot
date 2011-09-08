4bb10762a013a0ae932ccdc6ded5b258321ff8db
Jonas Forsberg <himynameisjonas@gmail.com>
2011-09-08 15:34:55 +0200

    added self update command

---
+++ lib/commands/selfupdate.rb
@@ -0,0 +1,19 @@
+class Selfupdate < Bot::Command
+  respond_to "selfupdate"
+  def self.respond(message)
+    system("git fetch")
+    diff = `git log --decorate --left-right --graph --cherry-pick --oneline master...origin/master`
+    if diff.empty?
+      message.send("No updates found. No need to reboot!")
+    else
+      message.send("Applying updates and rebooting:")
+      message.send(diff)
+      system("git pull --rebase")
+      exec("rake run &")
+    end
+  end
+  
+  def self.description
+    "Do a 'git pull' and restart Flower"
+  end
+end
