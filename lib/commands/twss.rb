9ede657df342630ba10374e651398922d7379378
Jonas Forsberg <himynameisjonas@gmail.com>
2011-09-10 11:01:22 +0200

    increased the twos threshold

--- lib/commands/twss.rb
+++ lib/commands/twss.rb
@@ -2,7 +2,7 @@ class Twss < Bot::Command
   monitor_all
   
   require 'twss'
-  TWSS.threshold = 1.0
+  TWSS.threshold = 3.0
   def self.respond(message)
     message.send("That's what she said!") if TWSS(message.message)
   end
