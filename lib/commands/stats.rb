5dec7098d1fd66c20c1929a9caf206ff2cf4bd6d
Jonas Forsberg <himynameisjonas@gmail.com>
2011-09-10 10:09:54 +0200

    added stats command: chartbeat's online right now

---
+++ lib/commands/stats.rb
@@ -0,0 +1,24 @@
+class Stats < Bot::Command
+  respond_to "online"
+  CHARTBEAT_URL = "http://api.chartbeat.com"
+  
+  def self.respond(message)
+    case message.command
+    when "online"
+      message.send "Online right now: #{online_right_now}"
+    end
+  end
+
+  def self.description
+    "Stats"
+  end
+
+  private
+
+  def self.online_right_now
+    require 'open-uri'
+    doc = open "#{CHARTBEAT_URL}/quickstats?host=#{Bot::Config.chartbeat.domain}&apikey=#{Bot::Config.chartbeat.key}"
+    json = JSON.parse doc.read
+    json["people"]
+  end
+end
