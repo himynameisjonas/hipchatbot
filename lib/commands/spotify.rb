9a7dd8ef7461e1defe0ec33e64a1b1345ddf4900
Jonas Forsberg <himynameisjonas@gmail.com>
2011-09-10 17:47:08 +0200

    Added more Spotify commands (SEARCH!!!)

--- lib/commands/spotify.rb
+++ lib/commands/spotify.rb
@@ -1,33 +1,76 @@
 class Spotify < Bot::Command
-  respond_to "next", "prev", "play", "pause", "track"
+  respond_to "next", "prev", "play", "pause", "playing", "track", "album"
   require 'appscript'
-  
+  require 'json'
+
   def self.respond(message)
     case message.command
     when "next"
       Appscript::app("Spotify").next_track
-      current_track = Appscript::app("Spotify").current_track
-      message.send("Now playing: #{current_track.name.get} - #{current_track.artist.get}")
+      message.send("Now playing: #{current_track}")
     when "prev"
       Appscript::app("Spotify").previous_track 
       sleep 0.1
       Appscript::app("Spotify").previous_track 
-      current_track = Appscript::app("Spotify").current_track
-      message.send("Now playing: #{current_track.name.get} - #{current_track.artist.get}")
+      message.send("Now playing: #{current_track}")
     when "pause"
       Appscript::app("Spotify").pause
       message.send("Stoped playing")
     when "play"
       Appscript::app("Spotify").play
-      current_track = Appscript::app("Spotify").current_track
-      message.send("Started playing: #{current_track.name.get} - #{current_track.artist.get}")
+      message.send("Started playing: #{current_track}")
+    when "playing"
+      message.send("Playing: #{current_track}")
     when "track"
-      current_track = Appscript::app("Spotify").current_track
-      message.send("Playing: #{current_track.name.get} - #{current_track.artist.get}")
+      return message.send "@#{message.from} please add a search query" unless message.message
+      play_href search_for_track message.message
+      hide_spotify
+      message.send("Now playing: #{current_track}")
+    when "album"
+      return message.send "@#{message.from} please add a search query" unless message.message
+      message.send "currently broken'"
+      play_href search_for_album message.message
+      hide_spotify
+      message.send("Now playing: #{current_track}")
     end
   end
 
   def self.description
     "Controll Spotify"
+  end
+
+  def self.current_track
+    current_track = Appscript::app("Spotify").current_track
+    "#{current_track.artist.get} - #{current_track.name.get}"
+  end
+
+  def self.hide_spotify
+    system "osascript", "-e", "tell application \"Finder\" to set visible of process \"Spotify\" to false"
+  end
+
+  def self.play_href(href)
+    system "open", href
+  end
+
+  def self.search_for_track(query)
+    response = JSON.parse open("http://ws.spotify.com/search/1/track.json?q=#{CGI.escape query}").read
+    track = check_availability(response["tracks"]).first
+    track["href"]
+  end
+
+  def self.search_for_album(query)
+    response = JSON.parse open("http://ws.spotify.com/search/1/album.json?q=#{CGI.escape query}").read
+    track = check_availability(response["albums"]).first
+    track["href"]
+  end
+
+  def self.check_availability(results)
+    results.select do |result|
+      if result.has_key? "album"
+        result["album"]["availability"]["territories"].include?("SE")
+      else
+        result["availability"]["territories"].include?("SE")
+      end
+    end
   end
 end
