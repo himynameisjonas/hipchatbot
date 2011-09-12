562fa1ceabcf20ed7fd6a7357f77e4a711a4dbc8
Jonas Forsberg <himynameisjonas@gmail.com>
2011-09-12 23:55:48 +0200

    Post larger images, upload to imgur for nice sizes

--- lib/commands/pic.rb
+++ lib/commands/pic.rb
@@ -2,6 +2,9 @@ class Pic < Bot::Command
   respond_to "pic"
   require 'open-uri'
   require 'json'
+  require 'httparty'
+
+  IMAGE_CACHE = {}
 
   def self.description
     "Post a picture from search string"
@@ -9,11 +12,22 @@ class Pic < Bot::Command
 
   def self.respond(message)
     begin
-      json = JSON.parse(open("http://api.search.live.net/json.aspx?AppId=#{Bot::Config.live_id}&Query=#{CGI.escape "\"#{message.message}\""}&Sources=Image&Version=2.0&Adult=Moderate&Image.Count=1").read)
-      image = json["SearchResponse"]["Image"]["Results"][0]["Thumbnail"]["Url"]
-      message.send("#{image}#.jpg")
+      image = IMAGE_CACHE.has_key?(message.message) ? IMAGE_CACHE[message.message] : upload_to_imgur(search_bing(message.message), message.message)
+      message.send_html("<a href='#{image["upload"]["links"]["original"]}'><img src='#{image["upload"]["links"]["large_thumbnail"]}'></a>")
     rescue NoMethodError
       message.send("sorry, no matches")
     end
+  end
+
+  def self.search_bing(query)
+    json = JSON.parse(open("http://api.search.live.net/json.aspx?AppId=#{Bot::Config.live_id}&Query=#{CGI.escape "\"#{query}\""}&Sources=Image&Version=2.0&Adult=Moderate&Image.Count=1").read)
+    json["SearchResponse"]["Image"]["Results"][0]["MediaUrl"]
+  end
+
+  def self.upload_to_imgur(url, query)
+    return IMAGE_CACHE[query] if IMAGE_CACHE.has_key? query
+    response = HTTParty.post("http://api.imgur.com/2/upload.json", :query => {:key => Bot::Config.imgur_api_key, :image => url}).parsed_response
+    IMAGE_CACHE[query] = response
+    response
   end
 end
